6b4eb389f4f3b6d455375d634146b017f6ebd5b7
manmadharaomaruvada <manmadharao94@gmail.com>
2023-02-22 14:24:32 +0530

    demo

---
+++ src/main/java/com/ojas/demo/Demo.java
@@ -0,0 +1,20 @@
+package com.ojas.demo;
+
+public class Demo {
+
+public static void pairDifference(int []arr,int target) {
+		
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[j] - arr[i] == target||arr[i] - arr[j] == target)
+					System.out.println("The Pair is :" + arr[i] + " " + arr[j]);
+			}
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { 1, 5, 2, 4, 8 };
+		 int target=3;
+		 pairDifference(arr, target);
+	}
+}
