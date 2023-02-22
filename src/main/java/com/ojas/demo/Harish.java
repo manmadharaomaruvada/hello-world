18eb9cdeae6435344f4c6c862cd33afd661633cb
Harish Mamidlala <hm22101@corp.ojas-it.com>
2023-02-22 15:59:57 +0530

    Created new Class Harish

---
+++ src/main/java/com/ojas/demo/Harish.java
@@ -0,0 +1,15 @@
+package com.ojas.demo;
+
+public class Harish {
+	public static void sum(int a,int b) {
+		int c= a+b;
+		System.out.println(c);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		sum(12,78);
+
+	}
+
+}
